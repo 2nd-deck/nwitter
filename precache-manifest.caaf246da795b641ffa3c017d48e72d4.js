77a0ab608224b9d04f268ab1be174223c185889c
2nd-deck <archness@naver.com>
2020-10-11 00:02:59 +0900

    Updates

---
+++ precache-manifest.caaf246da795b641ffa3c017d48e72d4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "005734fb93a7613d264d1f8ee276365d",
+    "url": "/nwitter/index.html"
+  },
+  {
+    "revision": "7c6180d90d46a089b5a4",
+    "url": "/nwitter/static/css/main.86fde8f1.chunk.css"
+  },
+  {
+    "revision": "40a6c0ddc9f89f7c5d74",
+    "url": "/nwitter/static/js/2.c5616597.chunk.js"
+  },
+  {
+    "revision": "2c0b89fe32f6d0b561deea7159cbd810",
+    "url": "/nwitter/static/js/2.c5616597.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7c6180d90d46a089b5a4",
+    "url": "/nwitter/static/js/main.749570cf.chunk.js"
+  },
+  {
+    "revision": "97fb235846154051176c",
+    "url": "/nwitter/static/js/runtime-main.222ae764.js"
+  }
+]);
